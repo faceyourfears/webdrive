49f0cf6d4868b1afe558921caaf64147ba681db6
faceyourfears <roisensthorn@gmail.com>
2023-04-25 16:56:24 +0800

    fetch but not quite

--- fetch.js
+++ fetch.js
@@ -1,28 +1,47 @@
-let http = new XMLHttpRequest();
-
-http.open('get', 'products.json', true);
-
-http.send();
-
-http.onload = function(){
-
-    if(this.readyState == 4 && this.status == 200){
-        let products = JSON.parse(this.responseText)
-
-        let output = "";
-
-        for(let item of products){
-            output += `
-                <div class="product">
-                    <img src="${item.image}">
-                    <h4>${item.name}</h4>
-                    <h5>${item.intensity}</h5>
-                        <div class="cart">
-                            <a href="#"><i class='bx bxs-heart'></i></a>
-                        </div>
-                </div>
-            `;
-        }
-        document.querySelector(".products").innerHTML = output;
+const products = [
+    {
+        "image": "images/joy.webp",
+        "name": "Joy",
+        "intensity": "High"
+    }, 
+    {
+        "image": "images/dune.webp",
+        "name": "Dune",
+        "intensity": "Low"
+    },
+    {
+        "image": "images/poison.webp",
+        "name": "Poison",
+        "intensity": "High"
+    },
+    {
+        "image": "images/gris_dior.webp",
+        "name": "Gris Dior",
+        "intensity": "Low"
+    },
+    {
+        "image": "images/miss_dior.webp",
+        "name": "Miss Dior",
+        "intensity": "High"
+    },
+    {
+        "image": "images/jadore.webp",
+        "name": "J'adore",
+        "intensity": "High"
     }
-}
+]
+
+    let output = "";
+
+    for(let item of products){
+        output += `<div class="product">
+                        <img src="${item.image}">
+                        <h4>${item.name}</h4>
+                        <h5>${item.intensity}</h5>
+                            <div class="cart">
+                                <a href="#"><i class='bx bxs-heart'></i></a>
+                            </div>
+                    </div>`;
+    }
+
+document.querySelector(".products").innerHTML = output;
